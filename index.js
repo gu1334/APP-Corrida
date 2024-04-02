13e90b765458a222489ddfb72ece4f817cc08c40
Gustavo Cadamuro <gustavo@appsafehouse.com.br>
2024-04-02 20:47:06 -0300

    pegando a geolocalização

--- index.js
+++ index.js
@@ -4,24 +4,39 @@ const rideListElement = document.querySelector("#rideList");
 // Obtém todas as viagens do armazenamento local
 const allRides = getAllrides();
 
-// Itera sobre todas as viagens
-allRides.forEach(([id, value]) => {
+// Itera sobre todas as viagens presentes no armazenamento local
+allRides.forEach(async ([id, value]) => {
     // Analisa o valor da viagem de volta para um objeto JavaScript
     const ride = JSON.parse(value);
     // Adiciona o ID da viagem ao objeto ride
     ride.id = id;
 
-    const firtPosition = ride.data[0]
-    console.log(firtPosition)
-    // Cria um novo elemento <li> para exibir o ID da viagem
+    // Obtém a primeira posição registrada na viagem
+    const firstPosition = ride.data[0];
+
+    // Obtém dados de localização da primeira posição
+    const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude);
+
+    // Cria um novo elemento <li> para exibir o ID da viagem e a localização
     const itemElement = document.createElement("li");
-    // Define o texto do elemento <li> como o ID da viagem
-    //itemElement.innerText = ride.id;
+
+    // Define o texto do elemento <li> como a cidade e o país da localização
+    itemElement.innerText = `${firstLocationData.city}-${firstLocationData.countryCode}`;
+
+    // Cria um elemento <div> para a cidade
+    const cityDiv = document.createElement("div");
+    // Define o texto da div como a cidade
+    cityDiv.innerText = firstLocationData.city;
+    // Adiciona a div à li
+    itemElement.appendChild(cityDiv);
+
     // Adiciona o elemento <li> à lista de viagens no DOM
     rideListElement.appendChild(itemElement);
 });
 
-
-function getLocationData(latitude, longitude){
-
+// Função assíncrona para obter dados de localização com base em latitude e longitude
+async function getLocationData(latitude, longitude) {
+    const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    const response = await fetch(url);
+    return await response.json();
 }
